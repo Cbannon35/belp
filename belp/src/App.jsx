ac1c893a7624c96659a1b00ec724f140e0743804
Nhu Vu <geniuslab00@gmail.com>
2023-04-21 20:55:04 -0700

    i love poland

--- belp/src/App.jsx
+++ belp/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import styled from "styled-components";
 import "./App.css";
+import Home from './pages/Home';
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <p>hi</p>
+      <Home/>
     </>
   );
 }
