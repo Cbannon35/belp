3fa59486301f95d01326e8ddc9550258de47b5e4
Christopher Bannon <bannon.c.35@berkeley.edu>
2023-04-21 20:57:14 -0700

    before nhu's changes

--- belp/src/App.jsx
+++ belp/src/App.jsx
@@ -1,17 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import styled from "styled-components";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+function Loading() {
   return (
     <>
-      <p>hi</p>
+      <p>loading!</p>
     </>
   );
+}
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [loadscreen, setLoadscreen] = useState(true);
+
+  // Display the loading screen for 5 seconds
+  useEffect(() => {
+    const id = setTimeout(() => {
+      setLoadscreen(false);
+    }, 5000);
+    // return () => {
+    //     clearTimeout(id)
+    // }
+  }, []);
+
+  return <>{loadscreen ? <Loading /> : <p>loaded!</p>}</>;
 }
 
 export default App;
