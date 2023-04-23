cef349fdb7c894705a9b3b9fbc5b0e22e99bb97d
Nhu Vu <geniuslab00@gmail.com>
2023-04-23 13:49:02 -0700

    edited homepage, removed some unnecessary default code

--- belp/src/pages/Home.jsx
+++ belp/src/pages/Home.jsx
@@ -1,5 +1,22 @@
-import React from "react";
+import React from "react"
+import styles from '../css/Home.module.css'
+import { useState } from 'react'
+import {FiSearch} from 'react-icons/fi'
+import user from '../assets/user.webp'
+
 
 export default function Home() {
-  return <div>Our lovely homepage</div>;
+  return (
+    <div>
+      <div className={styles.upper}>
+        <div className={styles.searchBar}>
+            <FiSearch className={styles.searchIcon}/>
+            <input className={styles.search} type="text"/>
+          </div>
+          <button className={styles.buttonContainer}>
+            <img src={user} className={styles.userButton}/>
+          </button>
+      </div>
+    </div>
+  )
 }
