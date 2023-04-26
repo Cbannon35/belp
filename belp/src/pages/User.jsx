6f5a4a9f2dbc1348a89a5cb2c27b4c42df292625
Nhu Vu <geniuslab00@gmail.com>
2023-04-25 19:43:24 -0700

    updated user page...barely :<

--- belp/src/pages/User.jsx
+++ belp/src/pages/User.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import styles from "../css/User.module.css"
+import userimg from "../assets/user.webp"
 
 export default function User() {
-  return <div>User</div>;
+  return (
+    <div className={styles.page}>
+      <div className={styles.imgContainer}>
+        <img src={userimg} className={styles.userImg}/>
+      </div>
+      <div className={styles.midSection}>
+        <div className={styles.infoContainer}>
+          <div className={styles.blueBox}>
+          </div>
+          <div className={styles.userInfo}>
+            <div className={styles.textInfo}>
+              Username
+            </div>
+            <div className={styles.pw}>
+              <div>Password</div>
+            </div>
+            <div className={styles.textInfo}>
+              Change password
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
