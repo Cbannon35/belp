71521431a55708f6f95f539b64f3e78be930a33d
Nhu Vu <geniuslab00@gmail.com>
2023-04-26 16:27:29 -0700

    updated some user page stuff/fonts

--- belp/src/pages/User.jsx
+++ belp/src/pages/User.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import styles from "../css/User.module.css"
 import userimg from "../assets/user.webp"
+import { useState } from 'react';
 
 export default function User() {
+  
+  const [selectPinned, setSelectPinned] = useState(true)
+  const [selectReviewed, setSelectReviewed] = useState(false)
+
   return (
     <div className={styles.page}>
       <div className={styles.imgContainer}>
@@ -28,20 +33,18 @@ export default function User() {
         </div>
       </div>
       <div className={styles.categories}>
-        <div className={styles.pinnedBox}>
-          <div className={styles.catText}>
-            Pinned
-          </div>
-        </div>
-        <div className={styles.reviewBar}>
-          <div className={styles.catText}>
-            Reviewed
-          </div>
+        <div className={styles.catBox}>
+         <button onClick={() => {setSelectPinned(true); setSelectReviewed(false)}} className={selectPinned ? styles.catSelected : styles.catText}>
+             Pinned
+         </button>
+         <button onClick={() => {setSelectReviewed(true); setSelectPinned(false)}} className={selectReviewed ? styles.catSelected : styles.catText}>
+             Reviewed
+         </button>
         </div>
       </div>
       <div className={styles.bathrooms}>
-        
       </div>
+     
     </div>
   );
 }
